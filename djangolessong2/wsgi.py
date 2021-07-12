08e3d634e5f0013adcca6cacb23ba990d4f8c049
lytzxp <lytzxp@gmail.com>
2021-07-13 00:18:22 +0800

    修改wsgi

--- djangolessong2/wsgi.py
+++ djangolessong2/wsgi.py
@@ -10,6 +10,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangolessong2.settings')
 application=Cling(get_wsgi_application())
 application = get_wsgi_application()
+if not settings.DEBUG:
+    try:
+        from dj_static import Cling
+        application = Cling(get_wsgi_application())
+    except:
+        pass
